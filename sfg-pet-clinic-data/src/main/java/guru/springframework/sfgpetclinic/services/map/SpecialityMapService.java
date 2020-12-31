0062a670a529c0e90fb7060eead6c5dd3f1d02bf
phyo-kyaw <phy0ky6w@gmail.com>
2020-12-31 15:46:55 +1100

    Created Specialty Map Service. closes #31

---
+++ sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.SpecialityService;
+
+import java.util.Set;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Long>
+                    implements SpecialityService {
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+}
