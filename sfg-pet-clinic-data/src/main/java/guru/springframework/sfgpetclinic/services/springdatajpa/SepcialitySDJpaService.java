e9af667d37c3ea5f25f2b95030258a669e5eedf2
phyo-kyaw <phy0ky6w@gmail.com>
2021-01-01 16:45:12 +1100

    Created Spring Data JPA Service to Specialties. closes #44

---
+++ sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/SepcialitySDJpaService.java
@@ -0,0 +1,44 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.repositories.SpecialityRepository;
+import guru.springframework.sfgpetclinic.services.SpecialityService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SepcialitySDJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SepcialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
